eedb7f82275d2eb53a2fd20aa47761a7eb56fc25
kravenoff42 <jcraven42@gmail.com>
2016-06-13 10:54:48 -0400

    Added done and delete buttons

--- Output/script.js
+++ Output/script.js
@@ -3,15 +3,16 @@ var main = function() {
 	var currentItem="";
 	var checkbox= '<a class="done"><i class="material-icons">&#xE876;</i></a>';
 	var deleteBtn= '<a class="delete"><i class="material-icons">&#xE872;</i></a>';
-	var editBtn= '<a class="edit"><i class="material-icons">&#xE150;</i></a>';
+//edit button to come
+//	var editBtn= '<a class="edit"><i class="material-icons">&#xE150;</i></a>';
 	$('form').submit(function(event) {
 
 		var $input = $(event.target).find('input');
 		var comment = $input.val();
 
 		if (comment != "") {
-			var html = $('<li>').html(checkbox + '<span>'+ comment +'</span>'+ deleteBtn + editBtn).attr('id', 'list'+count);
-			console.log(html);
+			var html = $('<li>').html(checkbox + '<span>'+ comment +'</span>' + deleteBtn).attr('id', 'list'+count);
+
 			html.appendTo('#com');
 			$input.val("");
 			count++;
@@ -19,17 +20,25 @@ var main = function() {
 		return false;
 	});
 
-	$('.delete').on('click', this.parent, function(currentItem) {
+	$(document).on('click', ".delete", function(currentItem) {
 		currentItem= $(this).parent();
-		console.log(currentItem);
 		$(currentItem).remove();
 	});
 
-	$('.done').click(function(){
-		currentItem= $(this).parent().attr('id');
-	console.log(currentItem);
-		$(currentItem).child('span').toggleClass('complete');
+	$(document).on('click', ".done", function(currentItem) {
+		currentItem= $(this).next();
+		$(this).toggleClass('checked');
+		$(currentItem).toggleClass('complete');
 	});
+/* Edit Function to come
+	$(document).on('click', ".edit", function(currentItem) {
+		currentItem= $(this).prev();
+
+		var text=$(currentItem).text();
+		var newInput = $('<input>').attr()
+		$().insertAfter('.done')
+	});
+	*/
 };
 
 $(document).ready(main);
