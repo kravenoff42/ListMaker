ebcaa04e693b8171b2d3c26fa575a953be853a98
kravenoff42 <jcraven42@gmail.com>
2016-06-13 09:31:17 -0400

    Added don, delete and edit buttons. Trying to hookup functions

--- Output/script.js
+++ Output/script.js
@@ -1,15 +1,35 @@
 var main = function() {
+	var count=2;
+	var currentItem="";
+	var checkbox= '<a class="done"><i class="material-icons">&#xE876;</i></a>';
+	var deleteBtn= '<a class="delete"><i class="material-icons">&#xE872;</i></a>';
+	var editBtn= '<a class="edit"><i class="material-icons">&#xE150;</i></a>';
 	$('form').submit(function(event) {
+
 		var $input = $(event.target).find('input');
 		var comment = $input.val();
-		
+
 		if (comment != "") {
-			var html = $('<li>').text(comment);
-			html.prependTo('#com');
+			var html = $('<li>').html(checkbox + '<span>'+ comment +'</span>'+ deleteBtn + editBtn).attr('id', 'list'+count);
+			console.log(html);
+			html.appendTo('#com');
 			$input.val("");
+			count++;
 		}
 		return false;
 	});
-}
+
+	$('.delete').on('click', this.parent, function(currentItem) {
+		currentItem= $(this).parent();
+		console.log(currentItem);
+		$(currentItem).remove();
+	});
+
+	$('.done').click(function(){
+		currentItem= $(this).parent().attr('id');
+	console.log(currentItem);
+		$(currentItem).child('span').toggleClass('complete');
+	});
+};
 
 $(document).ready(main);
